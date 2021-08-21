b86605156a9a3d7fc122bd401c7040cb386246ec
Jan Richie Yu <yujanrichie@gmail.com>
2021-08-21 09:21:08 +0800

    update service, test in role component

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { RoleUserInfo } from '../models/user-role-info';
+import { UserRoleService } from '../services/user-role.service';
 
 @Component({
   selector: 'app-roles',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RolesComponent implements OnInit {
 
-  constructor() { }
+  public roleUserList: RoleUserInfo[] = [];
+  private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private service: UserRoleService) {
+
+  }
 
   ngOnInit() {
+    this.service.loadData();
+
+    this.service.roleUserListObs.pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(list => {
+        this.roleUserList = list
+
+        //sort by name
+        this.roleUserList && this.roleUserList.sort((a, b) => a.name.localeCompare(b.name));
+      }
+    );
+  }
+
+  //note we call this on ngModelChange to detect each input right away
+  //we can call this on blur instead if we want to let the user type everything first
+  public updateRoleName(roleID: number, roleName: string) {
+    this.service.updateRoleName(roleID, roleName);
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next(true);
+    this.ngUnsubscribe.complete();
   }
 
 }
--- src/app/services/user-role.service.ts
+++ src/app/services/user-role.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, forkJoin, Observable } from 'rxjs';
 
-import { UserInfo, RoleInfo } from '../models/user-role-info';
+import { UserRoleInfo, UserInfo, RoleInfo, RoleUserInfo, BaseUserInfo } from '../models/user-role-info';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRoleService {
+
+  private isFirstLoad: boolean = true;
+
+  private userList: UserInfo[] = [];
+  private userRoleList: UserRoleInfo[] = [];
+  private userRoleListSbj: BehaviorSubject<UserRoleInfo[]> = new BehaviorSubject<UserRoleInfo[]>([]);
+  public userRoleListObs = this.userRoleListSbj.asObservable();
+  
+  private roleList: RoleInfo[] = [];
+  private roleUserList: RoleUserInfo[] = [];
+  private roleUserListSbj: BehaviorSubject<RoleUserInfo[]> = new BehaviorSubject<RoleUserInfo[]>([]);
+  public roleUserListObs = this.roleUserListSbj.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -31,6 +43,133 @@ export class UserRoleService {
 
   private getAllUsers(): Observable<UserInfo[]> {
     return this.get('users.json');
+  }
+
+  private loadAllUsersAndRoles() {
+    forkJoin(this.getAllRoles(), this.getAllUsers()).subscribe(result => {
+      if ((result != null) && (result.length > 0)) {
+        this.roleList = result[0];
+        this.userList = result[1];
+      }
+      //map the values and publish updated values
+      this.updateUsersAndRoles();
+    });
+  }
+
+  private updateUsersAndRoles() {
+    //remap
+    this.userRoleList = this.mapRolesToUsers(this.roleList, this.userList);
+    this.roleUserList = this.mapUsersToRoles(this.userList, this.roleList);
+
+    //publish updated values
+    if (this.userRoleListSbj != null) this.userRoleListSbj.next(this.userRoleList);
+    if (this.roleUserListSbj != null) this.roleUserListSbj.next(this.roleUserList);
+  }
+
+  private mapRolesToUsers(roles: RoleInfo[], users: UserInfo[]): UserRoleInfo[] {
+    let userRoleList: UserRoleInfo[] = [];
+
+    if (users != null) {
+      //go through each user and check their roles
+      users.forEach(user => {
+        if (user != null) {
+          //create a new UserRoleInfo object
+          let newUserRoleInfo: UserRoleInfo = {
+            id: user.id,
+            name: user.name
+          }
+         
+          //get the full RoleInfo for each user role id
+          if ((roles != null) && (user.roles != null)) {
+            let newRoleList: RoleInfo[] = [];
+
+            user.roles.forEach(userRoleID => {
+              let currentRoleInfo: RoleInfo = roles.find(role => role.id === userRoleID);
+              if (currentRoleInfo != null) newRoleList.push(currentRoleInfo);
+            });
+
+            //add roleList property to our UserRoleInfo object
+            newUserRoleInfo.roleList = newRoleList;
+          }
+          
+          userRoleList.push(newUserRoleInfo);
+        }
+      });
+    }
+
+    return userRoleList;
+  }
+
+  private mapUsersToRoles(users: UserInfo[], roles: RoleInfo[]): RoleUserInfo[] {
+    let roleUserList: RoleUserInfo[] = [];
+
+    if (roles != null) {
+      //go through each role and look for users from the users list
+      roles.forEach(role => {
+        if (role != null) {
+          //create a new RoleUserInfo object
+          let newRoleUserInfo: RoleUserInfo = {
+            id: role.id,
+            name: role.name,
+            colour: role.colour
+          }
+         
+          //get the BaseUserInfo for each user that has this role
+          if (users != null) {
+            let newUserList: BaseUserInfo[] = [];
+
+            users.forEach(user => {
+              if ((user != null) && (user.roles != null)) {
+                const isRoleInUser: boolean = user.roles.includes(role.id);
+                if (isRoleInUser) newUserList.push({ id: user.id, name: user.name });
+              }
+            });
+
+            //add userList property to our RoleUserInfo object
+            newRoleUserInfo.userList = newUserList;
+          }
+          
+          roleUserList.push(newRoleUserInfo);
+        }
+      });
+    }
+
+    return roleUserList;
+  }
+
+  public loadData() {
+    if (this.isFirstLoad) {
+      this.isFirstLoad = false;
+      this.loadAllUsersAndRoles();
+    }
+  }
+
+  public updateUserName(userID: number, userName: string) {
+    //update the user name in user list
+    if ((this.userList != null) && (this.userList.length > 0)) {
+      let currentUser = this.userList.find(user => user.id === userID);
+
+      if (currentUser != null) {
+        currentUser.name = userName;
+
+        //update the maps and publish updated values
+        this.updateUsersAndRoles();
+      }
+    }
+  }
+
+  public updateRoleName(roleID: number, roleName: string) {
+    //update the role name in role list
+    if ((this.roleList != null) && (this.roleList.length > 0)) {
+      let currentRole = this.roleList.find(role => role.id === roleID);
+
+      if (currentRole != null) {
+        currentRole.name = roleName;
+
+        //update the maps and publish updated values
+        this.updateUsersAndRoles();
+      }
+    }
   }
   
 }
